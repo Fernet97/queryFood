5ccb659cdbcc25bcd4e54578df86504261dd40b4
Fernet97 <44033607+Fernet97@users.noreply.github.com>
2021-06-18 19:20:28 +0200

    Configurazione package.json

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const csvtojson = require('csvtojson');
 require('dotenv').config()
 
 const port = process.env.PORT || 3000;
+const url = process.env.LOCAL_MONGO_CONNECTION;
+const CSVrecipeDetails = "./CulinaryDB/01_Recipe_Details.csv";
+
 
 const app = express()
 
-const url = process.env.LOCAL_MONGO_CONNECTION;
-
+//**** Connessione al DB *****
 const connectionParams={
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -23,16 +26,44 @@ mongoose.connect(url,connectionParams)
     })
 
 
-
-//Get the default connection
 var connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// ******************************************
 
+
+// *** Importa CSVs nel database ***
+var arrayToInsert = [];
+csvtojson().fromFile(CSVrecipeDetails).then(source => {
+    // Fetching the all data from each row
+    for (var i = 0; i < source.length; i++) {
+         var oneRow = {
+             Recipe_ID: source[i]["Recipe ID"],
+             Title: source[i]["Title"],
+             //Source: source[i]["Source"], // Non serve
+             Cuisine: source[i]["Cuisine"]
+         };
+         console.log(oneRow);
+         arrayToInsert.push(oneRow);
+     }
+
+    console.log("Importazione nel db ...");
+
+     //inserting into the table “employees”
+    var collectionName = 'Recipe';
+    var collection = connection.collection(collectionName);
+    collection.insertMany(arrayToInsert, (err, result) => {
+        if (err) console.log(err);
+        if(result){
+            console.log("Importazione CSV nel DB completata.");
+        }
+    });
+});
+// ******************************************
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('*** PROGETTO DB2 ***')
 })
 
 app.listen(port, () => {
