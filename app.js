af88521f3e40884da8351b544d8e0f3cf1f8a1fc
Fernet97 <44033607+Fernet97@users.noreply.github.com>
2021-06-18 17:55:13 +0200

    add Mongo and dotenv

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000;
+
+const app = express()
+
+const url = process.env.LOCAL_MONGO_CONNECTION;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 
-const port = 3000
+
+//Get the default connection
+var connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 
 
